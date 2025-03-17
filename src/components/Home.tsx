ccae59c93d008f9e9b26230d18b525db9514c4b6
Pravinsingh Rathod <156897820+Pravinrathod3@users.noreply.github.com>
2025-03-17 14:43:23 +0530

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -92,7 +92,7 @@ const Home = () => {
                 One Line at a Time! Be part of a community that pushes the boundaries of technology.
               </p>
               <a
-                href="#events"
+                href="https://dsawithjava.vercel.app/"
                 className="inline-flex items-center gap-2 bg-transparent text-primary px-4 sm:px-6 lg:px-8 py-2 sm:py-3 lg:py-4 rounded-lg font-semibold border-2 border-primary hover:bg-primary/10 transition-colors text-sm sm:text-base"
               >
                 Explore Events
